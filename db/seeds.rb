d301b4e0f93ef7d5c26be1b6c01c03b0e738a86f
Saad Ishtiaq <saad-ishtiaq@outlook.com>
2025-06-24 13:25:07 +0200

    feature-municipalities: updated seed file according to the new structure == Changes: - Prices without a municipality are assigned to the global municipality - Each price is linked to its respective package - amount_cents values in packages.yml are used as the latest prices for the packages

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,17 @@
 
 puts "Removing old packages and their price histories"
 Package.destroy_all
+Municipality.destroy_all
 
 puts "Creating new packages"
 
-Package.insert_all(
-  YAML.load_file(Rails.root.join("import/packages.yaml"))
-)
+packages_data = YAML.load_file(Rails.root.join("import/packages.yaml"))
+
+amount_cents_data = packages_data.map { |package| { name: package['name'], amount_cents: package.delete('amount_cents') } }
+
+Package.insert_all(packages_data)
+
+global_municipality = Municipality.find_or_create_by!(name: 'global')
 
 premium = Package.find_by!(name: "premium")
 plus = Package.find_by!(name: "plus")
@@ -16,6 +21,33 @@ basic = Package.find_by!(name: "basic")
 puts "Creating a price history for the packages"
 prices = YAML.load_file(Rails.root.join("import/initial_price_history.yaml"))
 
-premium.prices.insert_all(prices["premium"])
-plus.prices.insert_all(prices["plus"])
-basic.prices.insert_all(prices["basic"])
+def assign_global_municipality(prices)
+  prices.map do |price|
+    price['municipality'] ||= 'global'
+
+    municipality = Municipality.find_or_create_by!(name: price['municipality'])
+    price.merge(municipality_id: municipality.id)
+  end
+end
+
+premium_prices = assign_global_municipality(prices["premium"])
+plus_prices = assign_global_municipality(prices["plus"])
+basic_prices = assign_global_municipality(prices["basic"])
+
+premium.prices.insert_all(premium_prices.map { |price| price.except('municipality') })
+plus.prices.insert_all(plus_prices.map { |price| price.except('municipality') })
+basic.prices.insert_all(basic_prices.map { |price| price.except('municipality') })
+
+puts "Creating final prices based on amount_cents_data"
+amount_cents_data.each do |data|
+  package = Package.find_by!(name: data[:name])
+
+  municipality_id = global_municipality.id
+
+  Price.create!(
+    amount_cents: data[:amount_cents],
+    package: package,
+    municipality_id: municipality_id
+  )
+end
+puts "Finished seeding the database with packages and prices"
